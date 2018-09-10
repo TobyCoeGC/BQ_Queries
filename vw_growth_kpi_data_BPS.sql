5f4e1a32d7a8e89e3617bdd77f566354d3a076eb
TobyCoeGC <41323356+TobyCoeGC@users.noreply.github.com>
2018-09-10 18:09:54 +0100

    Update vw_growth_kpi_data_BPS.sql
    Most logic from here has now moved back into view vw_growth_kpi_data_bps_prediv.sql to amterialise an un-nested AGG struct. This view can now do the necessary 'SAFE DIVIDE' with all the correct values on all the correct rows (Post materialisation' of the upstream view.

--- vw_growth_kpi_data_BPS.sql
+++ vw_growth_kpi_data_BPS.sql
@@ -1,189 +1,21 @@
 -- vw_growth_kpi_data_by_partner.sql
 
--- reflatten fixed_scheme_mandates_created
- -- tested this against flattened aggregate and against the acid test of mandateSchemeFix "Pairing" recompressing the original rows with their fixed 'scheme/mandates_created' values
-WITH data_prep AS
-
-                (
-                SELECT
-                  DISTINCT
-                  parent.* EXCEPT (mandates_created, scheme, mandateSchemeFix, active, inactive, activated, monthly_fee_active)
-                , fix.mandates_created
-                , fix.scheme
-                --, -1 AS mandateSchemeFix -- to prove that this has worked, only values of -1 and 0 will come out in the final results
-                , CASE WHEN parent.DoubleCount_Fix=1 THEN parent.active                                       ELSE 0  END AS active
-                , CASE WHEN parent.DoubleCount_Fix=1 THEN parent.inactive                                     ELSE 0  END AS inactive
-                , CASE WHEN parent.DoubleCount_Fix=1 THEN parent.activated                                    ELSE 0  END AS activated
-                , CASE WHEN parent.DoubleCount_Fix=1 THEN parent.monthly_fee_active                           ELSE 0  END AS monthly_fee_active
+WITH Final AS
+               (
+                 SELECT
+                   pre_divide.* EXCEPT (Active)
+                 , IEEE_DIVIDE(ActiveDivider.ActiveConstant,ActiveDivider.ActiveDivider ) as Active
+                 FROM
+                 `{{ params.gbq_project_id }}.{{ params.gbq_dataset_materialized_views }}..vw_growth_kpi_data_bps_prediv` pre_divide
+              ) 
 
 
-                FROM
-                `gc-data-infrastructure-7e07.experimental_tables.vw_growth_kpi_data_by_partner_prep` fix
-
-                JOIN
-
-                `gc-data-infrastructure-7e07.experimental_tables.vw_growth_kpi_data_by_partner_prep` parent
-
-                ON
-                fix.kpi_day = parent.kpi_day
-                AND
-                fix.organisation_id = parent.organisation_id
-
-                WHERE
-                fix.MandateSchemeFix = 1
-                AND
-                parent.MandateSchemeFix = 0
-                and
-                parent.scheme IS NULL
-                and
-                parent.mandates_created IS NULL
-
-                UNION DISTINCT
-
-
-
-                SELECT
-                  DISTINCT
-                  parent.* EXCEPT (mandates_created, scheme, mandateSchemeFix, active, inactive, activated, monthly_fee_active)
-                , parent.mandates_created
-                , parent.scheme
-                --, parent.mandateSchemeFix
-                , CASE WHEN parent.DoubleCount_Fix=1 THEN parent.active                                       ELSE 0  END AS active
-                , CASE WHEN parent.DoubleCount_Fix=1 THEN parent.inactive                                     ELSE 0  END AS inactive
-                , CASE WHEN parent.DoubleCount_Fix=1 THEN parent.activated                                    ELSE 0  END AS activated
-                , CASE WHEN parent.DoubleCount_Fix=1 THEN parent.monthly_fee_active                           ELSE 0  END AS monthly_fee_active
-
-
-                FROM
-
-                `gc-data-infrastructure-7e07.experimental_tables.vw_growth_kpi_data_by_partner_prep` parent
-                LEFT JOIN
-                (
-                    SELECT
-                      kpi_day
-                    , organisation_id
-                    FROM
-
-                    `gc-data-infrastructure-7e07.experimental_tables.vw_growth_kpi_data_by_partner_prep`
-                    WHERE
-                    MandateSchemeFix = 1
-                ) fix
-                ON
-                fix.kpi_day = parent.kpi_day
-                AND
-                fix.organisation_id = parent.organisation_id
-
-                WHERE
-                parent.MandateSchemeFix = 0
-                and
-                fix.kpi_day IS NULL
-                and
-                fix.organisation_id IS NULL
-                )
-
-, psm AS
-              (
-                SELECT
-                  data_prep.*
-
-                , CASE WHEN psm.PSM IS NULL
-                            THEN 'unallocated'
-                        ELSE psm.PSM
-                   END                                                                      AS PartnerShipSuccessManager
-	              FROM
-                data_prep
-                LEFT JOIN
-                ( SELECT DISTINCT
-                     PSM
-                   , parent_app_name
-                   FROM
-                  `gc-data-infrastructure-7e07.experimental_tables.segmentation_mapping_partners`
-                 )  psm
-                ON
-                data_prep.signup_app_name = psm.parent_app_name
-              )
-
--- anomalous 2000 or so rows with double counting 'active' flag on an edgecase of null partner lines, caused by days where mandates are created on a scheme level, but there is no other activity on that day
-, doublecount_null_partner_dual_scheme AS
-                                          (
-                                               SELECT
-                                                 psm.*
-                                               , row_number () OVER (PARTITION BY organisation_id, kpi_day ORDER BY SCHEME) AS doublecount_null_fix
-                                               FROM
-                                               psm
-
-                                               JOIN
-                                              (
-                                                  SELECT
-                                                    organisation_id
-                                                  , kpi_day
-                                                  FROM
-                                                  `gc-data-infrastructure-7e07.experimental_tables.vw_growth_kpi_data_by_partner`
-                                                  WHERE
-                                                  doublecount_fix = 1
-                                                  AND
-                                                  scheme IS NOT NULL
-                                                  GROUP BY
-                                                    organisation_id
-                                                  , kpi_day
-
-                                                  HAVING count(*) >1
-                                              ) dupes
-
-                                              USING (  organisation_id, kpi_day )
-
-                                              UNION ALL
-
-                                              SELECT
-                                                 psm.*
-                                               , NULL AS doublecount_null_fix
-                                               FROM
-                                               psm
-
-                                               LEFT JOIN
-                                              (
-                                                  SELECT
-                                                    organisation_id
-                                                  , kpi_day
-                                                  FROM
-                                                  `gc-data-infrastructure-7e07.experimental_tables.vw_growth_kpi_data_by_partner`
-                                                  WHERE
-                                                  doublecount_fix = 1
-                                                  AND
-                                                  scheme IS NOT NULL
-                                                  GROUP BY
-                                                    organisation_id
-                                                  , kpi_day
-
-                                                  HAVING count(*) >1
-                                              ) dupes
-
-                                              USING (  organisation_id, kpi_day )
-                                              WHERE
-                                              dupes.organisation_id IS NULL
-                                              AND
-                                              dupes.kpi_day         IS NULL
-                                          )
-
-, final   AS                             (
-                                          SELECT
-                                            db.* EXCEPT (active)
-                                          , CASE
-                                                WHEN db.doublecount_null_fix = 1
-                                                      THEN 0
-                                                ELSE db.active
-                                            END                                     AS active
-
-                                          FROM
-                                          doublecount_null_partner_dual_scheme db
-
-                                         )
 
 SELECT
 DISTINCT
-  final.*
+  final.* EXCEPT (ActiveConstant,ActiveDivider)
 , wd.is_working_day
-, wd.working_day_of_month
+, wd.working_day_of_month 
 FROM
 final
 LEFT JOIN
