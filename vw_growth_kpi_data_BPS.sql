0cc9a7312357e9d3deed2c7b8eafbb9f8ff7bc0e
TobyCoeGC <41323356+TobyCoeGC@users.noreply.github.com>
2018-09-11 11:03:47 +0100

    Update vw_growth_kpi_data_BPS.sql
    Back to being able to do this in one step, the 'un-nested' schema that gets produced works fine to be able to do the row level maths to update back to the Active column on the 'parent' (nested) side of the data. SO effectively, we are 'un-nesting' on the fly, for the purposes of the calc...

--- vw_growth_kpi_data_BPS.sql
+++ vw_growth_kpi_data_BPS.sql
@@ -1,19 +1,141 @@
 -- vw_growth_kpi_data_by_partner.sql
 
-WITH Final AS
+--- reflatten fixed_scheme_mandates_created
+ -- tested this against flattened aggregate and against the acid test of mandateSchemeFix "Pairing" recompressing the original rows with their fixed 'scheme/mandates_created' values
+WITH data_prep AS
+
+                (
+                SELECT
+                  DISTINCT
+                  parent.* EXCEPT (mandates_created, scheme, mandateSchemeFix, active, inactive, activated, monthly_fee_active)
+                , fix.mandates_created
+                , fix.scheme
+                , CASE WHEN parent.DoubleCount_Fix=1 THEN parent.active                                       ELSE 0  END AS active
+                , CASE WHEN parent.DoubleCount_Fix=1 THEN parent.inactive                                     ELSE 0  END AS inactive
+                , CASE WHEN parent.DoubleCount_Fix=1 THEN parent.activated                                    ELSE 0  END AS activated
+                , CASE WHEN parent.DoubleCount_Fix=1 THEN parent.monthly_fee_active                           ELSE 0  END AS monthly_fee_active
+
+
+                FROM
+                `gc-data-infrastructure-7e07.experimental_tables.vw_growth_kpi_data_by_partner_prep` fix
+
+                JOIN
+
+                `gc-data-infrastructure-7e07.experimental_tables.vw_growth_kpi_data_by_partner_prep` parent
+
+                ON
+                fix.kpi_day = parent.kpi_day
+                AND
+                fix.organisation_id = parent.organisation_id
+
+                WHERE
+                fix.MandateSchemeFix = 1
+                AND
+                parent.MandateSchemeFix = 0
+                and
+                parent.scheme IS NULL
+                and
+                parent.mandates_created IS NULL
+
+                UNION DISTINCT
+
+
+
+                SELECT
+                  DISTINCT
+                  parent.* EXCEPT (mandates_created, scheme, mandateSchemeFix, active, inactive, activated, monthly_fee_active)
+                , parent.mandates_created
+                , parent.scheme
+                , CASE WHEN parent.DoubleCount_Fix=1 THEN parent.active                                       ELSE 0  END AS active
+                , CASE WHEN parent.DoubleCount_Fix=1 THEN parent.inactive                                     ELSE 0  END AS inactive
+                , CASE WHEN parent.DoubleCount_Fix=1 THEN parent.activated                                    ELSE 0  END AS activated
+                , CASE WHEN parent.DoubleCount_Fix=1 THEN parent.monthly_fee_active                           ELSE 0  END AS monthly_fee_active
+
+
+                FROM
+
+                `gc-data-infrastructure-7e07.experimental_tables.vw_growth_kpi_data_by_partner_prep` parent
+                LEFT JOIN
+                (
+                    SELECT
+                      kpi_day
+                    , organisation_id
+                    FROM
+
+                    `gc-data-infrastructure-7e07.experimental_tables.vw_growth_kpi_data_by_partner_prep`
+                    WHERE
+                    MandateSchemeFix = 1
+                ) fix
+                ON
+                fix.kpi_day = parent.kpi_day
+                AND
+                fix.organisation_id = parent.organisation_id
+
+                WHERE
+                parent.MandateSchemeFix = 0
+                and
+                fix.kpi_day IS NULL
+                and
+                fix.organisation_id IS NULL
+                )
+
+, psm AS
+              (
+                SELECT
+                  data_prep.*
+
+                , CASE WHEN psm.PSM IS NULL
+                            THEN 'unallocated'
+                        ELSE psm.PSM
+                   END                                      AS PartnerShipSuccessManager
+	              FROM
+                data_prep
+                LEFT JOIN
+                ( SELECT DISTINCT
+                    PSM
+                   ,parent_app_name
+                  FROM
+                 `gc-data-infrastructure-7e07.experimental_tables.segmentation_mapping_partners`
+                 )  psm
+                ON
+                data_prep.signup_app_name = psm.parent_app_name
+              )
+
+
+, ActiveDivider AS                        (                        
+                                            SELECT
+                                              CAST(COUNT(*) AS FLOAT64)        AS ActiveDivider
+                                            , CAST(MAX(IFNULL(Active,0))  AS FLOAT64)    AS ActiveConstant
+                                            , KPI_DAY
+                                            , organisation_id
+                                            FROM
+                                            psm
+                                            WHERE 
+                                            organisation_id IS NOT NULL
+                                            GROUP BY
+                                             KPI_DAY
+                                           , organisation_id 
+                                         )
+                                             
+
+, Final AS
                (
                  SELECT
                    pre_divide.* EXCEPT (Active)
                  , IEEE_DIVIDE(ActiveDivider.ActiveConstant,ActiveDivider.ActiveDivider ) as Active
                  FROM
-                 `{{ params.gbq_project_id }}.{{ params.gbq_dataset_materialized_views }}..vw_growth_kpi_data_bps_prediv` pre_divide
+                 psm pre_divide
+                 JOIN
+                 ActiveDivider
+                 USING
+                 (organisation_id, kpi_day)
               ) 
 
 
 
 SELECT
 DISTINCT
-  final.* EXCEPT (ActiveConstant,ActiveDivider)
+  final.* 
 , wd.is_working_day
 , wd.working_day_of_month 
 FROM
